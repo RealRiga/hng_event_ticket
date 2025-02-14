6ea5d243ec927e29218590da62a5d593862deae9
RealRiga <116930426+RealRiga@users.noreply.github.com>
2025-02-14 02:47:34 +0100

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './home';
 import UsersDetails from './usersDetails';
